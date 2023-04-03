19ed9076a4efe25cbfec7b3f7956279c998ef8b3
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-03 11:07:24 -0400

    Add Inter font and create custom document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+// import "https://rsms.me/inter/inter.css";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
