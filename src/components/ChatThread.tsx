a30f55387a14e61b23e1a9c5920e8a814d7121c8
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-05 09:54:29 -0400

    Revise chat content and adjust bubble spacing and sizing

--- src/components/ChatThread.tsx
+++ src/components/ChatThread.tsx
@@ -7,9 +7,9 @@ const chatMessages = [
     { id: 4, isAuthorUser: true, content: "Yes." },
     { id: 5, isAuthorUser: false, content: "Here is a list of similar jobs:" },
     { id: 6, isAuthorUser: false, content: "TODO - job list with expand button or pagination" },
-    { id: 7, isAuthorUser: false, content: "I've saved this list to your worksheets. Want me to alert you when there are updates?" },
-    { id: 8, isAuthorUser: true, content: "Sure." },
-    { id: 9, isAuthorUser: false, content: "Anything else?" },
+    { id: 7, isAuthorUser: false, content: "I've saved this list to your worksheets. Want me to reach out on your behalf?" },
+    { id: 8, isAuthorUser: true, content: <span>&#128077;</span> },
+    { id: 9, isAuthorUser: false, content: "Great! Anything else?" },
 ];
 
 // Filter out null and undefined values - https://www.skillthrive.com/hunter/snippets/combine-css-classes
@@ -20,15 +20,15 @@ function classNames(...classes: string[]) {
 export default function ChatThread() {
     return (
         <>
-            <main className="flex flex-col lg:h-screen lg:pl-72">
-                <div className="py-10 px-1 sm:px-4 xl:px-6 text-purple-950">
+            <main className="flex flex-col max-w-screen-xl lg:h-screen lg:pl-72">
+                <div className="py-10 px-1 sm:px-4 text-purple-950">
                     <ul role="list" className="space-y-3">
                         {chatMessages.map((message) => (
                             <li key={message.id}
                                 className={classNames(
                                     message.isAuthorUser
-                                    ? "bg-neutral-50 ml-16 mr-4 sm:ml-48 md:ml-64"
-                                    : "bg-pink-50 ml-4 mr-16 sm:mr-48 md:mr-64",
+                                    ? "bg-neutral-50 w-fit ml-auto mr-4"
+                                    : "bg-pink-50 w-fit ml-4 mr-auto",
                                     "overflow-hidden px-4 py-4 rounded-full sm:px-6"
                                 )}
                             >
