3a9b5c09d3e089ee61932f987c8048d82676c207
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-06 13:17:18 -0400

    Create JobList heading

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -8,7 +8,25 @@ const jobs = [
 
 export default function JobList() {
     return (
-        <div>
+        <>
+            <div className="border-b border-neutral-200 bg-white px-4 py-5 sm:px-6">
+                <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                    <div className="ml-4 mt-4">
+                        <h3 className="text-base font-semibold leading-6 text-purple-950">Job List</h3>
+                        <p className="mt-1 text-sm text-purple-800">
+                            Identify jobs that match and choose next steps.
+                        </p>
+                    </div>
+                    <div className="ml-4 mt-4 flex-shrink-0">
+                        <button type="button"
+                            className="relative inline-flex items-center rounded-md bg-pink-900 px-3 py-2 text-sm font-semibold text-white
+                            hover:bg-pink-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-800"
+                        >
+                            Request new list
+                        </button>
+                    </div>
+                </div>
+            </div>
             <ul role="list" className="divide-y divide-neutral-200">
                 {jobs.map((job) => (
                     <li key={job.id} className="py-4">
@@ -16,6 +34,6 @@ export default function JobList() {
                     </li>
                 ))}
             </ul>
-        </div>
+        </>
     );
 }
