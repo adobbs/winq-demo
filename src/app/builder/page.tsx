18de150878ca838f8557792b05359bda2414a941
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-08 07:11:17 -0400

    Upgrade to app directory

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { NextPage } from "next";
 import MainNav from "~/components/MainNav";
 import {
@@ -7,22 +9,22 @@ import {
     WrenchIcon,
 } from '@heroicons/react/24/outline'
 
-const marketplaceNav = [
+const builderNav = [
     { name: 'Conversations', href: '/conversations', icon: ChatBubbleLeftRightIcon, current: false },
-    { name: 'Bot Builder', href: '/builder', icon: WrenchIcon, current: false },
+    { name: 'Bot Builder', href: '/builder', icon: WrenchIcon, current: true },
     { name: 'Job Tracker', href: '/tracker', icon: BriefcaseIcon, current: false },
-    { name: 'Marketplace', href: '/marketplace', icon: BuildingStorefrontIcon, current: true },
+    { name: 'Marketplace', href: '/marketplace', icon: BuildingStorefrontIcon, current: false },
 ];
 
-const Marketplace: NextPage = () => {
+const Builder: NextPage = () => {
     return (
         <>
-            <MainNav page="Marketplace" navigation={marketplaceNav} />
+            <MainNav page="Bot Builder" navigation={builderNav} />
             <main className="py-10 lg:pl-72">
-                <div className="px-4 sm:px-6 lg:px-8 text-purple-950">TODO - Marketplace</div>
+                <div className="px-4 sm:px-6 lg:px-8 text-purple-950">TODO - Bot Builder</div>
             </main>
         </>
     );
 };
 
-export default Marketplace;
+export default Builder;
