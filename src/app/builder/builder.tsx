a6506f95d67f262b01584d138ad38d176e32f46d
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-09 12:18:23 -0400

    Create bot builder page stub

---
+++ src/app/builder/builder.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import MainNav from "~/components/MainNav";
+import {
+    BriefcaseIcon,
+    BuildingStorefrontIcon,
+    ChatBubbleLeftRightIcon,
+    WrenchIcon,
+} from '@heroicons/react/24/outline'
+import BotBuilder from "~/components/BotBuilder";
+
+const builderNav = [
+    { name: 'Conversations', href: '/conversations', icon: ChatBubbleLeftRightIcon, current: false },
+    { name: 'Bot Builder', href: '/builder', icon: WrenchIcon, current: true },
+    { name: 'Job Tracker', href: '/tracker', icon: BriefcaseIcon, current: false },
+    { name: 'Marketplace', href: '/marketplace', icon: BuildingStorefrontIcon, current: false },
+];
+
+export default function Builder() {
+    return (
+        <>
+            <MainNav page="Bot Builder" navigation={builderNav} />
+            <BotBuilder />
+        </>
+    );
+};
---
+++ src/components/BotBuilder.tsx
@@ -0,0 +1,12 @@
+export default function BotBuilder() {
+    return (
+        <>
+            <main className="flex flex-col max-w-screen-xl h-screen -mt-16 lg:mt-0 lg:pl-72">
+                <div className="py-10 px-2 sm:px-4 lg:px-6 mt-16 lg:mt-0 text-purple-950">
+                    <h1 className="text-2xl font-semibold">Bot Builder</h1>
+                    <div className="mt-1">Create a bot that will search for jobs and monitor opportunities for you. You can customize multiple bots to diversify your search.</div>
+                </div>
+            </main>
+        </>
+    );
+}
